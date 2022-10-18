59f8e5ab1c18b69755e82a4e5788234478b7f7cd
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-18 10:10:56 +0600

    The PHP echo Statement
    The echo statement can be used with or without parentheses: echo or echo().

--- indexed.php
+++ indexed.php
@@ -2,18 +2,11 @@
 <html>
     <body>
         <?php
-        function myTest(){
-          static $x = 0;
-          echo $x;
-          $x++;  
-        }
-
-        myTest();
-        echo "<br>";
-        myTest();
-        echo "<br>";
-        myTest();
-
+        
+            echo "<h2>PHP is Fun!</h2>";
+            echo "Hello world!<br>";
+            echo "I'm about to learn PHP!<br>";
+            echo "This ", "string ", "was ", "made ", "with multiple parameters. ";
         ?>
     </body>
 </html>
