ad089705c7a97f435f6a9ff9a1934797ee5e8b34
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-18 08:39:17 +0600

    Global and Local Scope

--- indexed.php
+++ indexed.php
@@ -1,16 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        $x = 5; // global scope
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+        function myText(){
+            // using x inside this function will generate an error
+            echo "<p>Variale x inside function is: $x</p>";
+        }
+        myText()
+        echo "<p>Variable x outside function is: $x</p>"
+        ?>
+    </body>
+</html>
