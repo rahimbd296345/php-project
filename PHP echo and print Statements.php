1073c54ebe5084b30ffd29c9c3498e66f3a9a692
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-18 10:16:42 +0600

    Display Variables
    The following example shows how to output text and variables with the echo statement:

--- PHP echo and print Statements.php
+++ PHP echo and print Statements.php
@@ -11,3 +11,22 @@ echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+?>
+
+</body>
+</html>
+
--- indexed.php
+++ indexed.php
@@ -3,10 +3,14 @@
     <body>
         <?php
         
-            echo "<h2>PHP is Fun!</h2>";
-            echo "Hello world!<br>";
-            echo "I'm about to learn PHP!<br>";
-            echo "This ", "string ", "was ", "made ", "with multiple parameters. ";
+            $txt1 = 'Learn PHP';
+            $txt2 = 'W3schools.com';
+            $x = 5;
+            $y = 4;
+
+            echo "<h2>" . $txt1 . "</h2>";
+            echo "Study PHP at " . $txt2 . "<br>";
+            echo $x + $y;
         ?>
     </body>
 </html>
