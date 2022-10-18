b59cf35f9952bb6a6aef44d204266a6efd92be94
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-18 10:26:11 +0600

    The PHP print Statement
    The print statement can be used with or without parentheses: print or print().

--- PHP echo and print Statements.php
+++ PHP echo and print Statements.php
@@ -30,3 +30,16 @@ echo $x + $y;
 </body>
 </html>
 
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+?> 
+
+</body>
+</html>
+
--- indexed.php
+++ indexed.php
@@ -2,15 +2,9 @@
 <html>
     <body>
         <?php
-        
-            $txt1 = 'Learn PHP';
-            $txt2 = 'W3schools.com';
-            $x = 5;
-            $y = 4;
-
-            echo "<h2>" . $txt1 . "</h2>";
-            echo "Study PHP at " . $txt2 . "<br>";
-            echo $x + $y;
+            print "<h2>PHP is Fun!</h2>";
+            print "Hello world!<br>";
+            print "I'm aboout to learn PHP!";
         ?>
     </body>
 </html>
