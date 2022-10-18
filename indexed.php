a49bdde1bdf2dc128218edfbdb3ba8a08406150e
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-18 09:14:52 +0600

    PHP The global Keyword

--- indexed.php
+++ indexed.php
@@ -2,14 +2,23 @@
 <html>
     <body>
         <?php
-        $x = 5; // global scope
+        /*$x = 5; // global scope
 
         function myText(){
             // using x inside this function will generate an error
             echo "<p>Variale x inside function is: $x</p>";
         }
         myText()
-        echo "<p>Variable x outside function is: $x</p>"
+        echo "<p>Variable x outside function is: $x</p>"*/
+
+        function myText() {
+            $x = 5; // Local scope
+            echo "<p>Variable x inside function is: $x</p>";
+        }
+        myText();
+
+        // using x outside the function will generate an error
+        echo "<p>Variable x outside function is: $x</p>";
         ?>
     </body>
 </html>
