3a732f81ce1e5925a54a47850617f359f2108cbe
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-18 07:54:58 +0600

    All update
    All update

---
+++ Case Sensitivity.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?> 
+
+</body>
+</html>
---
+++ indexed.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>My first PHP page</h1>
+
+        <?php
+            /*This is a multiple-lines comment block that spans over multipal lines*/
+
+
+
+        ?>
+
+    </body>
+</html>
+</html>
