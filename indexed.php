341730b05ec7e467ac14b283d65c90134d6f05f7
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-19 20:26:32 +0600

    PHP String
    A string is a sequence of characters, like "Hello world!".
    
    A string can be any text inside quotes. You can use single or double quotes:

--- indexed.php
+++ indexed.php
@@ -1,10 +1,25 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php
-            print "<h2>PHP is Fun!</h2>";
-            print "Hello world Rahim!<br>";
-            print "I'm aboout to learn PHP!";
-        ?>
-    </body>
-</html>
+<?php
+
+
+$x = "Hello world!";
+$y = 'Hellow world!';
+
+
+echo $x;
+echo $y;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+    
