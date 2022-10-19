0bcb845198b8561d057bf01a2f0f0f970521366e
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-19 20:21:52 +0600

    PHP Data Types
    PHP supports the following data types:
    
    String
    Integer
    Float (floating point numbers - also called double)
    Boolean
    Array
    Object
    NULL
    Resource

--- indexed.php
+++ indexed.php
@@ -3,7 +3,7 @@
     <body>
         <?php
             print "<h2>PHP is Fun!</h2>";
-            print "Hello world!<br>";
+            print "Hello world Rahim!<br>";
             print "I'm aboout to learn PHP!";
         ?>
     </body>
