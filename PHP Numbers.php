f81d2c37bab356eb68889b9dd230513060f68d83
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-20 21:21:46 +0600

    PHP Floats
    A float is a number with a decimal point or a number in exponential form

--- PHP Numbers.php
+++ PHP Numbers.php
@@ -16,3 +16,16 @@ var_dump(is_int($x));
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Check if the type of a variable is float 
+$x = 10.365;
+var_dump(is_float($x));
+?>  
+
+</body>
+</html>
--- indexed.php
+++ indexed.php
@@ -1,13 +1,9 @@
 <?php
 
-// Check if the type of a variable is integer
-$x = 5985;
-var_dump(is_int($x));
+// Check if the type of a variable is float
+$x = 10.365;
+var_dump(is_float($x));
 
-// Check again....
-
-$x = 59.85;
-var_dump(is_int($x));
 
 
 
