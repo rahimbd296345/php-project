ad322f52625fb0847c52c0c248f74896a9e996c5
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-22 08:28:43 +0600

    Add & remove files

---
+++ include/footer.php
@@ -0,0 +1,15 @@
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- jQuery Library -->
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<!-- Popper-JS -->
+<!-- <script src="node_modules/popper.js/dist/popper.min.js"></script> -->
+<!-- Bootstrap-JS -->
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Slick Carousel-JS -->
+<script src="node_modules/slick-carousel/slick/slick.js"></script>
+<!-- Magnific-popup-js-->
+<script src="js/jquery.magnific-popup.js"></script>
+<!-- Custom-JS -->
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="x-ua-compatible"
+          content="ie=edge"/>
+    <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta
+            name="description"
+            content="This is a Bootstrap 4 Quick Starter Pack."
+    />
+    <meta name="keyword"
+          content="bootstrap 4, quick starter pack"/>
+    <meta name="author"
+          content="Abdur Rahim"/>
+
+    <title>www.shatotacorporation.com</title>
+
+    <!--=== All CSS ===-->
+    <!-- Favicon here -->
+    <link rel="icon" href="./images/favicon.jpg" type="image/x-icon"/>
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
+          rel="stylesheet">
+    <!-- Bootstrap-CSS -->
+    <link
+            rel="stylesheet"
+            href="node_modules/bootstrap/dist/css/bootstrap.min.css"
+    />
+    <!-- Fontawesome-CSS -->
+    <link
+            rel="stylesheet"
+            href="node_modules/font-awesome/css/font-awesome.min.css"
+    />
+    <!-- SlickCarousel-CSS -->
+    <link rel="stylesheet"
+          href="node_modules/slick-carousel/slick/slick.css"/>
+    <!--Animate.css-->
+    <link rel="stylesheet"
+          href="node_modules/animate.css/animate.min.css"/>
+    <!--Maginfic-popup-CSS-->
+    <link rel="stylesheet"
+          href="assets/sass/magnific-popup.css"/>
+    <!-- Default-CSS -->
+    <link rel="stylesheet"
+          href="assets/sass/default.css"/>
+    <!-- Custom-CSS -->
+    <link rel="stylesheet"
+          href="assets/sass/main.css"/>
+    <!-- Responsive-CSS -->
+    <link rel="stylesheet"
+          href="assets/sass/responsive.css"/>
+</head>
+<body>
+
+
+<!-- header start here -->
+<h1 class="text-bg-dark">Good Morning...</h1>
+<!-- header start here -->
+
