64169e94ade6c533d72bc9e0cda851a7c659c4f9
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-22 08:40:55 +0600

    add

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
 ?>
 
 <?php 
-$x = "Abdur Rahim";
-$y = 'Hello world!';
-
-echo $x;
-echo "<br>"; 
-echo $y;
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?>
 
 
