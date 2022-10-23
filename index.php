4d0d27e4156554e9e17ef67dfda1eab099967fb5
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-23 09:29:19 +0600

    PHP round() Function
    The round() function rounds a floating-point number to its nearest integer:

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 <?php 
       echo "I'm about to learn PHP with vs code!<br>";
 
-   echo(sqrt(64) . "<br>");
-   echo(sqrt(0). "<br>");
-   echo(sqrt(1) . "<br>");
-   echo(sqrt(0));
+   echo(round(0.60). "<br>");
+   echo(round(0.50). "<br>");
+   echo(round(0.49). "<br>");
+   echo(round(-4.40). "<br>");
+   echo(round(-4.60));
 
 
 ?>
