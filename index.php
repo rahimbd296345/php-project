e601913ce7d1b75f61b2cc7ffc9dad41fb079123
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-23 08:14:17 +0600

    PHP Casting Strings and Floats to Integers

--- index.php
+++ index.php
@@ -3,7 +3,21 @@
 ?>
 
 <?php 
-echo "I'm about to learn PHP with vs code!<br>";
+      echo "I'm about to learn PHP with vs code!<br>";
+
+      // Cast float to int
+      $x = 23465.768;
+      $int_cast = (int)$x;
+      echo $int_cast;
+
+      echo "<br>";
+
+      // Cast string to int
+      $x = "23456.768";
+      $int_cast = (int)$x;
+      echo $int_cast;
+
+
 ?>
 
 
