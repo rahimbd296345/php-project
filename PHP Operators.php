9863f27e59d07514c4c9604710e0aafeabaa087f
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-24 09:29:21 +0600

    PHP Assignment Operators
    The PHP assignment operators are used with numeric values to write a value to a variable

--- PHP Operators.php
+++ PHP Operators.php
@@ -67,3 +67,29 @@ echo $x ** $y;
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 10;  
+echo $x;
+?>  
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 20;  
+$x += 100;
+
+echo $x;
+?>  
+
+</body>
+</html>
