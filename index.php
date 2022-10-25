6a96afa441dd1c89df796d2c16b71cf73beecafe
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-25 20:41:39 +0600

    The PHP switch Statement
    Use the switch statement to select one of many blocks of code to be executed.

--- index.php
+++ index.php
@@ -3,22 +3,12 @@
 ?>
 
 <?php
-echo '<h1>PHP - The if...elseif...else Statement:..</h1>';
+echo '<h1>PHP switch Statement:..</h1>';
 echo "<br>";
 
 
 
-$t = date('H');
-echo "<p>The hour (of the server) is " . $t;
-echo ", and will give the following message:</p>";
 
-if ($t < '10'){
-      echo 'Have a good morning!';
-} elseif ($t < '18'){
-      echo "Have a good day!";
-} else {
-      echo "Have a good night";
-}
 
 
 
