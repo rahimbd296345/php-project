34616a152aacbc190a98778b43680a5318937410
rahimbd296345 <99087922+rahimbd296345@users.noreply.github.com>
2022-10-25 15:23:03 +0600

    Rename new file PHP Conditional Statements

--- index.php
+++ index.php
@@ -3,26 +3,12 @@
 ?>
 
 <?php
-echo '<h1>Now Learning PHP Conditional Assignment Operators:</h1>';
+echo '<h1>PHP Conditional Statements:</h1>';
 echo "<br>";
 
 
 
-echo $status = (empty($user)) ? "anonymous" : "Logged in";
 
-echo ("<br>");
-$user = "Abdur Rahim";
-
-echo $status = (empty($user)) ? "anonymous": "logged in";
-
-echo ("<br>");
-
-
-
-echo $user = $_GET['user']?? 'anonymous';
-echo ('<br>');
-
-echo $color = $color ?? "red";
 
 
 
